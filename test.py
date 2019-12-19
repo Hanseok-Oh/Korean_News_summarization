002bb02f59bcfbfd1c612c25b9c5683837db55dd
Hanseok-Oh <ehfekfl9136@naver.com>
2019-12-19 20:28:49 +0900

    etc ; update several files in .gitignore

--- test.py
+++ test.py
@@ -1,23 +1,9 @@
+import re
 import pandas as pd
 import os
-# filename = os.getcwd()+'/data/{}/crawling.xlsx'.format('한화생명')
-filename = os.getcwd()+'/data/{}/contents.txt'.format('한화생명')
-# df = pd.read_excel(filename,index_col =0)
-df = pd.read_csv(filename, sep='\t')
-
-query = '한화생명'
-result_path = os.getcwd()+'/data'
-def excel_make(query):
-    data = pd.read_csv(result_path + '/{}/contents.txt'.format(query), sep='\t', header=None,
-                       error_bad_lines=False)
-    data.columns = ['years', 'company', 'title', 'contents', 'link']
-    out_file = result_path + '/{}/'.format(query) + 'crawling.xlsx'
-    data.to_excel(out_file, encoding='utf-8')
-    return
-
-excel_make(query)
-# for i in range(len(df)):
-#     print("{} line:".format(i), len(df.iloc[i,:]))
-
-# print(df.columns)
-# print(df.head())
+# print(os.getcwd().replace('\\','/'))
+df =pd.read_excel(os.getcwd().replace('\\','/')+'/data/카이스트/crawling.xlsx')
+for i in range(10):
+    temp =df.loc[i,'contents']
+    temp2= re.sub('[^\w. ]','',temp)
+    print(temp2,"\n")
